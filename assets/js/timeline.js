1f1cc1ba655de7d478927ad5676e6034409d527d
Wendy Reyes <wendy.reyessan@gmail.com>
2017-03-10 18:33:58 -0500

    agregando estilos

--- assets/js/timeline.js
+++ assets/js/timeline.js
@@ -1,4 +1,3 @@
-
 window.addEventListener('load',function() {
 
   var postButton = document.getElementById('publicar');
@@ -7,7 +6,7 @@ window.addEventListener('load',function() {
 // https://www.w3schools.com/jsref/dom_obj_textarea.asp
 
                var text = document.getElementById("post_input").value;
-               var editar = document.getElementById('rditar');
+
                var amigos = [];
                var publico = [];
 
@@ -15,12 +14,19 @@ window.addEventListener('load',function() {
                if (text == "") {alert("No has ingresado ningun texto");
              }else{
                document.getElementById("posts").innerHTML +=
-               "<div class=\'post\' contenteditable=\'false\'>"+text+
+               "<div class=\'post\' contenteditable=\'false\' style=\'border:1px solid blue\'>"+text+
                "<br><a href=\'#\' class=\'editor\'>Editar</a><a href=\'#\'>Eliminar</a></div>";
               }
 
-                  editar.addEventListener('click',function() {
-                    document.getElementByClassName('post').contentEditable = true;
-                  });
+              var editar = document.getElementsByClassName('editor');
+              editar.addEventListener('click',function() {
+                document.getElementsByClassName('post').contenteditable = true;
+               document.getElementsByClassName('post').designMode='on';
+              });
+
+
          });
+
+
+
 });
