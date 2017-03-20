ad64a02cce5db094570ec0373b6d530269986f46
flordeciruela <wendy.reyessan@gmail.com>
2017-03-19 23:28:43 -0500

    objeto post creado

--- assets/js/timeline.js
+++ assets/js/timeline.js
@@ -1,112 +1,97 @@
 
     var contador = 0;
+    var arrayPosts = new Array();
 
-  document.getElementById('publicar').addEventListener('click',function(event) {
-    event.preventDefault();
+    function Post(privacy, text){
 
-    var text = document.getElementById("post_input").value;
-    var privacy = document.getElementById('privacy').value;
-    var posts = document.getElementById('posts');
+     this.privacy = privacy;
+     this.text = text;
+     this.crear = function(){
 
-        if (text == "") {alert("No has ingresado ningun texto");
-        }else{
+                             var nuevoDiv = document.createElement('div');
+                                            if (this.privacy == "amigos") {
+                                                nuevoDiv.setAttribute("class", "amigos");
+                                            } else {
+                                                nuevoDiv.setAttribute("class", "publico");
+                                            }
 
-           var nuevoDiv = document.createElement('div');
-                          if (privacy == "amigos") {
-                              nuevoDiv.setAttribute("class", "amigos");
-                          } else {
-                              nuevoDiv.setAttribute("class", "publico");
-                          }
+                             var nuevaTextarea = document.createElement('textarea');
+                             var br = document.createElement('br');
 
-           var nuevaTextarea = document.createElement('textarea');
-           var br = document.createElement('br');
-
-           nuevaTextarea.value=text;
-           nuevaTextarea.readOnly = "true";
-
-           var editar = document.createElement('a');
-           editar.setAttribute('href',"#");
-           editar.innerHTML = "Editar";
-
-                         editar.addEventListener('click',function() {
-                         contador++
-                         if (contador % 2 != 0) {
-                             nuevaTextarea.removeAttribute('readonly');
-                             editar.innerHTML = "Guardar";
-                         }else{
+                             nuevaTextarea.value=this.text;
                              nuevaTextarea.readOnly = "true";
+
+                             var editar = document.createElement('a');
+                             editar.setAttribute('href',"#");
                              editar.innerHTML = "Editar";
-                             }
-                         });
+                             var eliminar = document.createElement('a');
+                             eliminar.setAttribute('href',"#");
+                             eliminar.innerHTML = "Eliminar";
 
-           var eliminar = document.createElement('a');
-           eliminar.setAttribute('href',"#");
-           eliminar.innerHTML = "Eliminar";
+                             //Eventos para editar y eliminar posts:
+                                   editar.addEventListener('click',function() {
+                                   contador++
+                                   if (contador % 2 != 0) {
+                                       nuevaTextarea.removeAttribute('readonly');
+                                       editar.innerHTML = "Guardar";
+                                   }else{
+                                       nuevaTextarea.readOnly = "true";
+                                       editar.innerHTML = "Editar";
+                                       }
+                                   });
 
-                    eliminar.addEventListener('click',function() {
-                         if(confirm("¿Estas seguro de querer eliminar este post?") == true) {
-                            posts.removeChild(nuevoDiv); }
-                    });
+                                   eliminar.addEventListener('click',function() {
+                                        if(confirm("¿Estas seguro de querer eliminar este post?") == true) {
+                                           posts.removeChild(nuevoDiv); }
+                                   });
 
+                             nuevoDiv.appendChild(nuevaTextarea);
+                             nuevoDiv.appendChild(br)
+                             nuevoDiv.appendChild(editar);
+                             nuevoDiv.appendChild(eliminar);
+                             return nuevoDiv;
+                             };
 
+     }
 
-           nuevoDiv.appendChild(nuevaTextarea);
-           nuevoDiv.appendChild(br);
-           nuevoDiv.appendChild(editar);
-           nuevoDiv.appendChild(eliminar);
-           posts.appendChild(nuevoDiv);
+    document.getElementById('publicar').addEventListener('click',function(event) {
+      event.preventDefault();
 
-        }
+      var text = document.getElementById("post_input").value;
+      var privacy = document.getElementById('privacy').value;
+      var posts = document.getElementById('posts');
 
-        document.getElementById("caja").reset();
+          if (text == "") {alert("No has ingresado ningun texto");
+          }else{
 
-  });
+                arrayPosts.push(new Post(privacy, text));  //inserta objeto nuevo en Array
+                posts.appendChild(arrayPosts[arrayPosts.length -1].crear());
+               }
 
-  var fPublico = document.getElementById('publico');
-  fPublico.addEventListener("click",function(){
+          document.getElementById("caja").reset();
+    });
+
+//Funciones para filtrar posts:
+  var showPublic = document.getElementById('publico');
+  showPublic.addEventListener("click",function(){
     var divs = document.getElementsByTagName('div');
-    for(var i = 0;i<divs.length;i++){
-      if(divs[i].classList == "publico"){
-        divs[i].style.display="block";
-      }
-      else {
-        divs[i].style.display="none"
-      }
-    }
+        for(var i in divs){
+            if(divs[i].classList == "publico"){
+              divs[i].style.display="block";
+              } else {
+              divs[i].style.display="none"
+              }
+          }
   })
 
-
-  var fAmigos = document.getElementById('amigos');
-  fAmigos.addEventListener("click",function(){
+  var showFriends = document.getElementById('amigos');
+  showFriends.addEventListener("click",function(){
     var divs = document.getElementsByTagName('div');
-    for(var i = 0;i<divs.length;i++){
-      if(divs[i].classList == "amigos"){
-        divs[i].style.display="block";
-      }
-      else {
-        divs[i].style.display="none"
-      }
-    }
+        for(var i in divs){
+            if(divs[i].classList == "amigos"){
+              divs[i].style.display="block";
+              } else {
+              divs[i].style.display="none"
+              }
+           }
   })
-
-/*
-           function CrearPost(privacidad, text){
-
-            this.privacidad = privacidad;
-            this.text = text;
-            this.crear = function(){
-
-                                    var nuevoDiv = document.createElement('div');
-                                                   if (privacy == "amigos") {
-                                                       nuevoDiv.setAttribute("class", "amigos");
-                                                   } else {
-                                                       nuevoDiv.setAttribute("class", "publico");
-                                                   }
-
-
-
-                                    return nuevoDiv;
-                                    };
-
-            }
-            */
